d4f53d6361b23c1f2d4f1b4fbf8d6f3ce2c43347
cdadar <gunbanmi@gmail.com>
2016-10-25 16:38:59 +0800

    modified yas and  add vcs custom.el

---
+++ personal/custom.el
@@ -0,0 +1,22 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(delete-selection-mode nil)
+ '(markdown-command
+   "~/.cabal/bin/pandoc -f markdown -t html -s --highlight-style espresso
+")
+ '(org-agenda-files
+   (quote
+    ("~/data/org-model/projects.org" "~/data/org-model/plan.org")))
+ '(package-selected-packages
+   (quote
+    (helm-ag helm-descbinds helm-projectile helm zop-to-char zenburn-theme yari yaml-mode which-key web-mode vue-mode volatile-highlights vlf vimrc-mode undo-tree solarized-theme smex smartrep smartparens smart-mode-line slime scss-mode ruby-tools rainbow-mode rainbow-delimiters ov org-pomodoro operate-on-number nodejs-repl move-text markdown-mode+ magit json-mode js2-refactor js-comint inf-ruby imenu-anywhere ido-ubiquitous hungry-delete helm-core guru-mode grizzl god-mode gitignore-mode gitconfig-mode git-timemachine gist geiser flycheck flx-ido expand-region ensime emmet-mode elisp-slime-nav easy-kill discover-my-major diminish diff-hl crux counsel-projectile company-tern company-auctex company-anaconda cider chinese-fonts-setup cdlatex browse-kill-ring beacon anzu ag ace-window)))
+ '(vlf-application (quote dont-ask)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
